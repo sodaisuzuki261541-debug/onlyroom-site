b73563074f7a395dd4fcca8750053cd8dcd6506b
sodaisuzuki261541-debug <sodai.suzuki_261541@glv.co.jp>
2026-05-01 17:06:42 +0900

    Create page.js

---
+++ app/app/app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-neutral-950 text-white font-sans">
+      <h1>Only Room</h1>
+      <p>東京・埼玉対応 / 家具セット販売・設置専門</p>
+    </div>
+  );
+}
